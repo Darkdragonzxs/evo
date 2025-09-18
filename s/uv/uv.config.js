d4a6b3fc2cd50af0604d6340ce38b39317ea318d
ZXS <DonaldBorthwick125@gmail.com>
2025-09-18 13:56:33 -0500

    Update uv.config.js

--- s/uv/uv.config.js
+++ s/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-	prefix: "/uv/evo/",
+	prefix: "/s/uv/evo/",
 	bare: "https://v4.mathsjunction.in/tspmo/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
